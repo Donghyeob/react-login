e811a9f645aea2e5d45bba83bbda17de31cd62ac
Donghyeob Kang <donghyeob.k@gmail.com>
2021-09-09 14:55:49 +0900

    input

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../Css/loginForm.css'
+import { UserOutlined, EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons'
 
 const LoginForm = () => {
+  const [id, setId] = useState()
+  const [password, setPassword] = useState()
+  const [passShow, setPassShow] = useState(false)
+
+  const onChangeId = (e) => {
+    const inputText = e.target.value
+
+    setId(inputText)
+  }
+
+  const onChangePassword = (e) => {
+    const inputText = e.target.value
+
+    setPassword(inputText)
+  }
+
   return (
-    <>
+    <div className='loginContainer'>
       <div>
         <h2>로그인</h2>
         <span>사용자 아이디와 암호를 입력하세요.</span>
       </div>
-      <div>
-
+      <div className='idContainer'>
+        <input className='idBox' type='text' value={id} onChange={onChangeId} placeholder='Id' />
+        <UserOutlined />
+      </div>
+      <div className='passContainer'>
+        <input className='pwdBox' type='password' value={password} onChange={onChangePassword} placeholder='Password' />
+        {passShow ? <EyeInvisibleOutlined /> : <EyeOutlined />}
       </div>
       <div>
         captcha
@@ -17,7 +40,7 @@ const LoginForm = () => {
         Login button
         remember id
       </div>
-    </>
+    </div>
   )
 }
 
