277204dff39f1b41a88c02546f5d3338ed87ca9a
Donghyeob Kang <donghyeob.k@gmail.com>
2021-11-11 16:48:28 +0900

    -

--- src/Components/ChangePassInput.js
+++ src/Components/ChangePassInput.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import VisibleInput from './VisibleInput'
 
 const ChangePassInput = ({ rePass, setRePass, confirmPass, setConfirmPass }) => {
